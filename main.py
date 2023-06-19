39f125af09605b7c9e4ae4c7aa66e2e2b27d2fe4
Celsia Starflare <88888339+OsmondFan@users.noreply.github.com>
2023-06-19 18:57:58 +0800

    Added resolution adjust

--- main.py
+++ main.py
@@ -2,14 +2,33 @@
 import os
 import librosa
 import soundfile as sf
+import sounddevice as sd
+import scipy.io.wavfile as wav
 
 # Define the folder paths
-dataset_raw_path = ''
-dataset_clean_path = ''
+dataset_raw_path = "Celsiav2/dataset_clean"
+dataset_clean_path = "Celsiav2/dataset_raw"
 
 # Define the minimum and maximum duration of each audio slice in seconds
 min_duration = 2
 max_duration = 10
+
+# Define the sound quality options
+sound_quality = "original" # Choose from "low", "medium", "uncompressed" or "sounddevice"
+if sound_quality == "low":
+    format = "OGG"
+    subtype = "FLOAT16"
+elif sound_quality == "medium":
+    format = "FLAC"
+    subtype = "FLOAT32"
+elif sound_quality == "high":
+    format = "WAV"
+    subtype = "DOUBLE"
+elif sound_quality == "original":
+    format = None # Use NumPy arrays and scipy to save as WAV files
+    subtype = None
+else:
+    raise ValueError("Invalid sound quality option")
 
 # Create the clean folder if it does not exist
 if not os.path.exists(dataset_clean_path):
@@ -31,9 +50,14 @@ for file in os.listdir(dataset_raw_path):
             duration = (end - start) / sr
             # Check if the duration is within the range
             if min_duration <= duration <= max_duration:
-                # Save the slice to the clean folder with a new name
-                new_name = file[:-4] + "_slice_" + str(i+1) + ".wav"
-                sf.write(os.path.join(dataset_clean_path, new_name), slice, sr)
+                # Save the slice to the clean folder with a new name and format
+                new_name = file[:-4] + "_slice_" + str(i+1)
+                if format is None:
+                    # Use scipy to save as WAV files with NumPy arrays
+                    wav.write(os.path.join(dataset_clean_path, new_name + ".wav"), sr, slice)
+                else:
+                    # Use soundfile to save as other formats with subtypes
+                    sf.write(os.path.join(dataset_clean_path, new_name + "." + format.lower()), slice, sr, format=format, subtype=subtype)
             elif duration < min_duration:
                 # Skip the slice if it is too short
                 continue
@@ -48,12 +72,22 @@ for file in os.listdir(dataset_raw_path):
                     small_duration = len(small_slice) / sr
                     # Check if the smaller slice fits into the range
                     if min_duration <= small_duration <= max_duration:
-                        # Save the smaller slice to the clean folder with a new name
-                        new_name = file[:-4] + "_slice_" + str(i+1) + "_" + str(j+1) + ".wav"
-                        sf.write(os.path.join(dataset_clean_path, new_name), small_slice, sr)
+                        # Save the smaller slice to the clean folder with a new name and format
+                        new_name = file[:-4] + "_slice_" + str(i+1) + "_" + str(j+1)
+                        if format is None:
+                            # Use scipy to save as WAV files with NumPy arrays
+                            wav.write(os.path.join(dataset_clean_path, new_name + ".wav"), sr, small_slice)
+                        else:
+                            # Use soundfile to save as other formats with subtypes
+                            sf.write(os.path.join(dataset_clean_path, new_name + "." + format.lower()), small_slice, sr, format=format, subtype=subtype)
                     elif small_duration > max_duration:
                         # Make a longest split that fits into the range and delete the rest of the slice
                         longest_slice = small_slice[:int(max_duration*sr)]
-                        new_name = file[:-4] + "_slice_" + str(i+1) + "_" + str(j+1) + ".wav"
-                        sf.write(os.path.join(dataset_clean_path, new_name), longest_slice, sr)
+                        new_name = file[:-4] + "_slice_" + str(i+1) + "_" + str(j+1)
+                        if format is None:
+                            # Use scipy to save as WAV files with NumPy arrays
+                            wav.write(os.path.join(dataset_clean_path, new_name + ".wav"), sr, longest_slice)
+                        else:
+                            # Use soundfile to save as other formats with subtypes
+                            sf.write(os.path.join(dataset_clean_path, new_name + "." + format.lower()), longest_slice, sr, format=format, subtype=subtype)
                         break
