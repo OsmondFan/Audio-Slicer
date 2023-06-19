bfbd22eda066ba587e283ec96b7fab86ba55d73a
Celsia Starflare <88888339+OsmondFan@users.noreply.github.com>
2023-06-19 19:09:17 +0800

    Mono or stereo both ok

--- main.py
+++ main.py
@@ -6,29 +6,32 @@ import sounddevice as sd
 import scipy.io.wavfile as wav
 
 # Define the folder paths
-dataset_raw_path = "Celsiav2/dataset_clean"
-dataset_clean_path = "Celsiav2/dataset_raw"
+dataset_raw_path = "Celsia/dataset_raw"
+dataset_clean_path = "Celsia/dataset_clean"
 
 # Define the minimum and maximum duration of each audio slice in seconds
 min_duration = 2
 max_duration = 10
 
 # Define the sound quality options
-sound_quality = "original" # Choose from "low", "medium", "uncompressed" or "sounddevice"
+sound_quality = "sounddevice" # Choose from "low", "medium", "uncompressed" or "sounddevice"
 if sound_quality == "low":
     format = "OGG"
     subtype = "FLOAT16"
 elif sound_quality == "medium":
     format = "FLAC"
     subtype = "FLOAT32"
-elif sound_quality == "high":
+elif sound_quality == "uncompressed":
     format = "WAV"
     subtype = "DOUBLE"
-elif sound_quality == "original":
+elif sound_quality == "sounddevice":
     format = None # Use NumPy arrays and scipy to save as WAV files
     subtype = None
 else:
     raise ValueError("Invalid sound quality option")
+
+# Define the stereo option
+stereo = True # Set to True or False
 
 # Create the clean folder if it does not exist
 if not os.path.exists(dataset_clean_path):
@@ -46,6 +49,13 @@ for file in os.listdir(dataset_raw_path):
         for i, (start, end) in enumerate(intervals):
             # Extract the slice of audio
             slice = audio[start:end]
+            # Check if stereo option is enabled
+            if stereo:
+                # Convert the slice to mono if it has more than one channel
+                if slice.ndim > 1:
+                    slice = librosa.to_mono(slice)
+                # Convert the slice to stereo by duplicating the channel
+                slice = librosa.to_stereo(slice)
             # Calculate the duration of the slice in seconds
             duration = (end - start) / sr
             # Check if the duration is within the range
@@ -68,6 +78,13 @@ for file in os.listdir(dataset_raw_path):
                 for j in range(num_slices):
                     # Extract the smaller slice of audio
                     small_slice = slice[j*slice_length:(j+1)*slice_length]
+                    # Check if stereo option is enabled
+                    if stereo:
+                        # Convert the small slice to mono if it has more than one channel
+                        if small_slice.ndim > 1:
+                            small_slice = librosa.to_mono(small_slice)
+                        # Convert the small slice to stereo by duplicating the channel
+                        small_slice = librosa.to_stereo(small_slice)
                     # Calculate the duration of the smaller slice in seconds
                     small_duration = len(small_slice) / sr
                     # Check if the smaller slice fits into the range
